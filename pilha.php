caa2524157a5b8722ccfd2afd6b08e6ffa4788ba
Felipe Silva <felipe.dacs@gmail.com>
2021-03-03 12:29:52 -0300

    :tv: add aula 1

--- pilha.php
+++ pilha.php
@@ -3,13 +3,20 @@
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
-    funcao2();
+    try {
+        funcao2();
+    } catch (RuntimeException $problema) {
+        echo "Na função 1, eu resolvi o problema da função 2" . PHP_EOL;
+    }
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 'Valor';
+//    $divisao = intdiv(5, 0);
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
