18271f29bfd647cff98b4b4b2f28bdd634d0cf13
Felipe Silva <felipe.dacs@gmail.com>
2021-03-03 16:40:29 -0300

    :tv: add aula 15

--- erros.php
+++ erros.php
@@ -9,6 +9,18 @@ error_reporting(E_ALL);
 // ini_set('error_log', 'var/log/minha-aplicacao'); // env prod
 ini_set('display_errors', 1); // env dev
 
+/* conversão de errors em exceptions, ideal para tratar na aplicação */
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    switch ($errno) {
+        case E_WARNING:
+            echo "Aviso: Isso é perigoso";
+            break;
+        case E_NOTICE:
+            echo "Melhor não fazer isso";
+            break;
+    }
+});
+
 /* E_ERROR */
 echo $variavel;
 echo $array[12];
