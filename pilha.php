a37dd35b5779885640242abde37709f1f9316152
Felipe Silva <felipe.dacs@gmail.com>
2021-03-03 12:32:57 -0300

    :tv: add aula 2

--- pilha.php
+++ pilha.php
@@ -7,6 +7,8 @@ function funcao1()
         funcao2();
     } catch (RuntimeException $problema) {
         echo "Na função 1, eu resolvi o problema da função 2" . PHP_EOL;
+    } catch (DivisionByZeroError $erro) {
+        echo "Erro ao dividir um número por 0" . PHP_EOL;
     }
     echo 'Saindo da função 1' . PHP_EOL;
 }
@@ -14,9 +16,9 @@ function funcao1()
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+    $divisao = intdiv(5, 0);
     $arrayFixo = new SplFixedArray(2);
     $arrayFixo[3] = 'Valor';
-//    $divisao = intdiv(5, 0);
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
