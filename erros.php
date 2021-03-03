3a6db3f77b2c0eed7678706fb095b3069aa98799
Felipe Silva <felipe.dacs@gmail.com>
2021-03-03 16:37:36 -0300

    :tv: add aula 14

--- erros.php
+++ erros.php
@@ -2,6 +2,13 @@
 
 // https://www.php.net/manual/en/errorfunc.constants.php
 
+
+// ideal estar no php.ini
+error_reporting(E_ALL);
+// ini_set('display_errors', 0); // env prod
+// ini_set('error_log', 'var/log/minha-aplicacao'); // env prod
+ini_set('display_errors', 1); // env dev
+
 /* E_ERROR */
 echo $variavel;
 echo $array[12];
